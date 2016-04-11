e171a2b914eb4f759fc38936378a1ab68366c6d5
Caparrini <Caparrini@users.noreply.github.com>
2016-04-11 18:27:32 +0200

    Initial Commit

---
+++ ECO/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ECO
+//
+//  Created by Antonio Caparrini on 11/4/16.
+//  Copyright (c) 2016 Antonio Caparrini. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
